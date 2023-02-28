e2e39cb4cae8c2bbb2b96cefccc9456cd8dc25f7
Hossein-Olfat <h1382olfat@gmail.com>
2023-02-28 06:15:02 -0800

    structure of project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import {Container} from './container/container.js';
+
 
 function App() {
   return (
     <div className="App">
-      
+        <Container/>      
     </div>
   );
 }
---
+++ src/container/container.js
@@ -0,0 +1,10 @@
+import './container.css';
+function Container(){
+    return (
+        <div className='container'>
+            I am container
+        </div>
+    )
+}
+
+export {Container};
